118f795c6d497b5af505fc5d05374b069def1c10
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-10-23 11:33:32 +0700

    Menambahkan komponen Text untuk menampilkan string prodi dengan font 35sp dan bold

--- app/src/main/java/com/example/questadvancedlayout_071/UIPertama.kt
+++ app/src/main/java/com/example/questadvancedlayout_071/UIPertama.kt
@@ -3,10 +3,14 @@ package com.example.questadvancedlayout_071
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ActivitasPertama(modifier: Modifier){
@@ -14,5 +18,11 @@ fun ActivitasPertama(modifier: Modifier){
     Column(modifier = Modifier.padding(top = 100.dp)
         .fillMaxSize(),
     horizontalAlignment = Alignment.CenterHorizontally
-    ){ }
+    ){
+
+        Text(text = stringResource(id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold)
+    }
+
 }
