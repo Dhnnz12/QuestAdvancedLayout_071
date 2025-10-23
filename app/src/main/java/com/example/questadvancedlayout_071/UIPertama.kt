54ed58ab62e30aca3dcf077e68c8f297dfffd716
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-10-23 11:43:32 +0700

    Menambahkan Spacer selebar 30dp

--- app/src/main/java/com/example/questadvancedlayout_071/UIPertama.kt
+++ app/src/main/java/com/example/questadvancedlayout_071/UIPertama.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -51,6 +52,7 @@ fun ActivitasPertama(modifier: Modifier){
                     modifier = Modifier.size(
                         size = 100.dp).padding(all = 5.dp)
                 )
+                Spacer(modifier = Modifier.width(width = 30.dp))
         }
 
 
@@ -58,4 +60,3 @@ fun ActivitasPertama(modifier: Modifier){
     }
     }
 
-}
